982f019af9bbc0d445bcbd0b04b9ead038439f2d
Adam Kim <akim456@gmail.com>
2021-12-09 14:42:40 -0500

    Initial commit

---
+++ #CPPA CPRA GDPR Study Game in Python.py
@@ -0,0 +1,12 @@
+#CPPA CPRA GDPR Study Game in Python
+#The purpose of this repo is to practice learn more about 
+#  -California Consumer Privacy Act (CCPA)
+#  -The California Privacy Rights Act (CPRA)
+#  -General Data Protection Regulation (GDPR)
+#  -Practice basic python
+
+
+#By: Adam Kim 
+#Github: www.github.com/resv
+#linkedin: www.linkedin/in/AdamKim456
+print("hello world")
---
+++ README.md
@@ -0,0 +1,2 @@
+# DATA-SECURITY-COMPLIANCE-WITH-PYTHON
+ To learn more about CPPA, CPRA, & GDPR , all while practicing python fundamentals
